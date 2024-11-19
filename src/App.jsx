ac181ef75d6a65dce61f1aec5522f5e65b815152
farhan0212 <farhan.ramadan0212@gmail.com>
2024-11-19 12:44:13 +0700

    penambahan animasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 import { useState } from "react";
 
@@ -20,6 +22,7 @@ function App() {
 
   useEffect(() => {
     getQuotes();
+    AOS.init();
   }, []);
 
   return (
@@ -27,6 +30,9 @@ function App() {
       {quotes.map((quote, index) => (
         <div
           key={index}
+          data-aos="fade-up"
+          data-aos-delay={index * 500}
+          data-aos-duration="1000"
           className="bg-white border-4 border-black shadow max-w-md p-6 mx-auto transition-all duration-300 ease-in-out hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[4px] hover:translate-y-[4px] focus-within:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] focus-within:translate-x-[4px] focus-within:translate-y-[4px] my-4">
           <h2 className="text-2xl font-black mb-4 uppercase">{quote.author}</h2>
           <p className="text-large">{quote.quote}</p>
